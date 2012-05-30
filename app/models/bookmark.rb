705e2d4f60dd0313826e31d188a473c645b2cf72
Yoshioka Tsuneo (吉岡 恒夫) <yoshiokatsuneo@gmail.com>
2012-05-30 07:28:30 +0300

    Added/implement relation between bookmarks and tags model.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,7 @@
 class Bookmark < ActiveRecord::Base
-  attr_accessible :url
+  attr_accessible :url, :tags_attributes
+  has_many :tags
+  
+  accepts_nested_attributes_for :tags, :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
